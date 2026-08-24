ce9a543ee66b88afb5bd1c4a5424df85c59b393f
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-24 17:48:53 +0200

    Completed folder 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  const filteredArr = arr.filter(x => !args.includes(x));
+
+  return filteredArr;
 };
 
 // Do not edit below this line
