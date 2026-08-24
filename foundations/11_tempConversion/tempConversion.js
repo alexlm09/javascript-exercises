25f14bb562186b6a4c6d6b931b1f75c3a8695b8d
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-24 18:20:35 +0200

    Completed folder 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  const tempCels = parseFloat(((tempFahr - 32) * (5/9)).toFixed(1));
+  return tempCels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCels) {
+  const tempFahr = parseFloat((tempCels * (9/5) + 32).toFixed(1));
+  return tempFahr;
 };
 
 // Do not edit below this line
