2b40ad3ff286e6458ac8714c3c61eb98c0d41b34
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-24 18:05:29 +0200

    Completed folder 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+  if (!Number.isInteger(int1) | !Number.isInteger(int2) | int1 < 0 | int2 < 0) {
+    return 'ERROR';
+  }
 
+  const num1 = (int1 <= int2) ? int1 : int2;
+  const num2 = (int2 >= int1) ? int2 : int1;
+  const rangeInts = [];
+
+  for (let i = num1; i <= num2; i++) {
+    rangeInts.push(i);
+  }
+
+  const sumInts = rangeInts.reduce((total, curr) => total + curr);
+  return sumInts;
 };
 
 // Do not edit below this line
