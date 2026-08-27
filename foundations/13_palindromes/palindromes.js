c4c386f8f3c391af1c6bbbadc94150a5d97901ab
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-27 06:59:42 +0200

    Completed folder 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const cleanedStr = str
+                        .toLowerCase()
+                        .split('')
+                        .filter(letter => alphanumerical.includes(letter))
+                        .join('');
 
+  const strL = cleanedStr.length;
+  const midL = Math.floor(strL/2);
+  
+  for (let i = 0; i <= midL; i++) {
+    if (cleanedStr[i] !== cleanedStr[strL-i-1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // Do not edit below this line
