cc304bae8a91419bcd5447b3170ffda4eb6c2ea2
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-27 06:44:47 +0200

    Completed folder 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currNum) => total + currNum , 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currNum) => total * currNum , 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a;
+	for (let i = 1; i < b; i++) {
+    total *= a;
+  }
+
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
