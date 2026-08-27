ab22e76024676c02af1f5a0ea445cdab3d645da2
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-27 07:41:15 +0200

    Completed folder 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((total, curr, index) => {
+    const yearDeath = curr.yearOfDeath ?? new Date().getFullYear();
+    curr['yearOfDeath'] = yearDeath;
+    const yearsOld = yearDeath - curr.yearOfBirth;
 
+    if (index === 0) {
+      return curr;
+    }
+
+    if (yearsOld > (total.yearOfDeath - total.yearOfBirth)) {
+      return curr;
+    }
+
+    return total;
+  }, {});
 };
 
 // Do not edit below this line
