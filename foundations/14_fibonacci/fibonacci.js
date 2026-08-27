3490bc55382b833015a3681c03f80e4014ed6fd1
alexlm09 <110418483+alexlm09@users.noreply.github.com>
2026-08-27 07:18:42 +0200

    Completed folder 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+  if (+index < 0) {
+    return "OOPS";
+  } else if (+index === 0) {
+    return 0;
+  }
+
+  let lastNum = 0;
+  let currentNum = 1;
+
+  for (let i = 2; i <= +index; i++) {
+
+    let sum = lastNum + currentNum;
+    lastNum = currentNum;
+    currentNum = sum;
+  }
+
+  return currentNum;
 };
 
 // Do not edit below this line
